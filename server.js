f60bc7e2a60200370256fabf31ff36248ee03abe
Nathan Friedly <nathan@nfriedly.com>
2013-03-05 19:58:13 -0500

    more pretty, finished removing jade

--- server.js
+++ server.js
@@ -5,28 +5,21 @@ var app = express();
 
 app.configure(function() {
 	app.use(express.static(__dirname + '/public'));
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
 	app.use(require('less-middleware')({ src: __dirname + '/public' }));
 });
 
 app.configure('production', function() {
 	app.enable('trust proxy');
 	app.use(express.compress());
-	app.use(function(req, res, next) {
-		if (req.host == process.env.BASE_DOMAIN) {
-			res.redirect(req.protocol + "://www." + req.host + req.originalUrl);
-		} else {
-			next();
-		}
-	});
 });
 
-app.get('/', function(req, res){
-	res.render('index');
-});
 
-app.get('/pagerank', function(req, res) {
+//var index = require('fs').readFileSync('./views/index.html').toString();
+//app.get('/', function(req, res){
+//	res.send(index);
+//});
+
+function getPr(req, res, js) {
 	if (!req.query.url) {
 		res.redirect('/');
 	}
@@ -42,34 +35,28 @@ app.get('/pagerank', function(req, res) {
 			data = data.error = err;
 			console.log('Error looking up pagerank', err);
 		}
-		
-		var formats = {
-			text: function(){
-				res.type('.txt').send(text_pr);
-			},
-			html: function(){
-				res.render('pagerank', data);
-			},
-			json: function(){
-				res.jsonp(data);
-			},
-			js: function(){
-				res.type('.js').send(util.format(
-					'alert("Pagerank of %s\\n\\n%s\\n\\nProvided by %s");',
-					url,
-					text_pr,
-					req.host
-				));
-			}
-		};
-		
-		if (formats[req.query.format]) {
-			formats[req.query.format]();
+		if (js) {
+			res.type('.js').send(util.format(
+				'alert("Pagerank of %s\\n\\n%s\\n\\nProvided by %s");',
+				url,
+				text_pr,
+				req.host
+			));
 		} else {
-			res.format(formats);
+			res.json(data);
 		}
 	});
+}
+
+app.get('/pagerank', function(req, res) {
+	getPr(req, res, false);
 });
+
+// bookmarklett
+app.get('/pagerank.js', function(req, res) {
+	getPr(req, res, true);
+});
+
 
 var port = process.env.PORT || 3000;
 app.listen(port);
