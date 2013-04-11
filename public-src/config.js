4959dbe255b0f6c78698a50ea74304701a91ed64
Nathan Friedly <nathan@nfriedly.com>
2013-04-10 20:21:53 -0400

    simplifying js config so that browserify doesn't trick it into thinking it's in a dev server

--- public-src/config.js
+++ public-src/config.js
@@ -1,19 +1,17 @@
+var isDev = (typeof location != 'undefined' && location.hostname == 'localhost');
 
+var devConfig = {
+    STRIPE_KEY: 'pk_test_UISfg44mvvob3QnCVacGMQQc',
+    SITE: 'http://localhost:3000',
+    SECURE_SITE: 'http://localhost:3000'
+};
 
-    var isDev = (typeof location != 'undefined' && location.hostname == 'localhost');
+var prodConfig = {
+    STRIPE_KEY: 'pk_live_jIp2eWrXfTNSUWtDFWMuIiK3', // this is the public ("publishable") key
+    SITE: 'http://pagerank.nfriedly.com',
+    SECURE_SITE: 'https://googlepagerank.herokuapp.com'
+};
 
-    var devConfig = {
-        STRIPE_KEY: 'pk_test_UISfg44mvvob3QnCVacGMQQc',
-        SITE: 'http://localhost:3000',
-        SECURE_SITE: 'http://localhost:3000'
-    };
+var config = isDev ? devConfig : prodConfig;
 
-    var prodConfig = {
-        STRIPE_KEY: 'pk_live_jIp2eWrXfTNSUWtDFWMuIiK3', // this is the public ("publishable") key
-        SITE: 'http://pagerank.nfriedly.com',
-        SECURE_SITE: 'https://googlepagerank.herokuapp.com'
-    };
-
-    var config = isDev ? devConfig : prodConfig;
-
-        module.exports = config;
+module.exports = config;
