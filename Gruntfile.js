b3fd797c239619728630259dd113181a098b6b3c
Nathan Friedly <nathan@nfriedly.com>
2014-06-12 16:15:33 -0400

    Revert "tweaking handling of public folder" - forgot that I need it on heroku & therfore it needs to be in git (or else generated at startup)
    This reverts commit edb2d760f35878a9a0acfe7fdb8195a7415121ea.

--- Gruntfile.js
+++ Gruntfile.js
@@ -169,6 +169,9 @@ module.exports = function(grunt) {
                 stderr: true,
                 failOnError: true
             },
+            'git-commit-public': {
+                command: 'git commit public/ -m "committing public files for deployment: ' + (new Date()) + '"'
+            },
             'heroku-push': {
                 command: 'git push heroku'
             },
@@ -203,7 +206,7 @@ module.exports = function(grunt) {
             },
             'prod-css-js': {
                 upload: [{
-                    src: 'public/*.{js,css,map}',
+                    src: 'public/*.{js,css}',
                     dest: '/'
                 }]
             },
@@ -244,7 +247,7 @@ module.exports = function(grunt) {
 
     grunt.registerTask('predeploy', ['clean:pre-deploy', 'jsbeautifier', 'jshint', 'csslint', 'copy:prod', 'build-html', 'cssmin:prod', 'browserify2:compile', 'uglify:prod']);
 
-    grunt.registerTask('deploy', ['predeploy', 'shell:heroku-push', 'shell:github-push', 's3', 'clean:post-deploy']);
+    grunt.registerTask('deploy', ['predeploy', 'shell:git-commit-public', 'shell:heroku-push', 'shell:github-push', 's3', 'clean:post-deploy']);
 
     grunt.registerTask('pretty', 'jsbeautifier'); // I always misspell "jsbeautifier"
 };
