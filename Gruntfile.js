edb2d760f35878a9a0acfe7fdb8195a7415121ea
Nathan Friedly <nathan@nfriedly.com>
2014-06-12 15:48:07 -0400

    tweaking handling of public folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -169,9 +169,6 @@ module.exports = function(grunt) {
                 stderr: true,
                 failOnError: true
             },
-            'git-commit-public': {
-                command: 'git commit public/ -m "committing public files for deployment: ' + (new Date()) + '"'
-            },
             'heroku-push': {
                 command: 'git push heroku'
             },
@@ -206,7 +203,7 @@ module.exports = function(grunt) {
             },
             'prod-css-js': {
                 upload: [{
-                    src: 'public/*.{js,css}',
+                    src: 'public/*.{js,css,map}',
                     dest: '/'
                 }]
             },
@@ -247,7 +244,7 @@ module.exports = function(grunt) {
 
     grunt.registerTask('predeploy', ['clean:pre-deploy', 'jsbeautifier', 'jshint', 'csslint', 'copy:prod', 'build-html', 'cssmin:prod', 'browserify2:compile', 'uglify:prod']);
 
-    grunt.registerTask('deploy', ['predeploy', 'shell:git-commit-public', 'shell:heroku-push', 'shell:github-push', 's3', 'clean:post-deploy']);
+    grunt.registerTask('deploy', ['predeploy', 'shell:heroku-push', 'shell:github-push', 's3', 'clean:post-deploy']);
 
     grunt.registerTask('pretty', 'jsbeautifier'); // I always misspell "jsbeautifier"
 };
